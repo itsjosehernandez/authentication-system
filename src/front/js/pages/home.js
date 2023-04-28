2e383525e6b72a9a41fd9de7236bfca8ec35b441
jgomez2011 <jesus.gomez0911@gmail.com>
2023-04-28 23:23:17 +0000

    parte de la backend

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,10 +4,8 @@ import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
-
-	return (
-		<div className="text-center mt-5">
+  return (
+    <div className="text-center mt-5">
 			<h1>Hello Rigo!!</h1>
 			<p>
 				<img src={rigoImageUrl} />
@@ -22,5 +20,5 @@ export const Home = () => {
 				</a>
 			</p>
 		</div>
-	);
+  );
 };
