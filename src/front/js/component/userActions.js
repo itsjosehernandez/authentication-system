167608fe94870356e246883bbd089ab7b465eeb9
itsjosehernandez <joseher2702@gmail.com>
2023-05-13 21:30:08 +0000

    actualizacion offcanvas

--- src/front/js/component/userActions.js
+++ src/front/js/component/userActions.js
@@ -23,37 +23,53 @@ export const UserActions = () => {
 				<h4 className="text-center">Name</h4>
 				<h6 className="text-center">{store.user}</h6>
 
+
+
 				<ul className="navbar-nav flex-grow-1 pe-3 text-start py-3">
 
-					<div className="container text-center">
-						<div className="row">
-							<div className="col">
-								<li className="nav-item">
-									<Link to={"/pages/home"} className="nav-link active " aria-current="page" >Tienda <i className="fa-solid fa-store"></i></Link>
-								</li>
-							</div>
-							<div className="col">
+					<div class="container text-center">
+						<div class="row">
+							{/* <div class="col">
+								 <li className="nav-item">
+									<Link to={"/pages/home"} className="nav-link active " aria-current="page" >Tienda <i class="fa-solid fa-store"></i></Link>
+								</li> 
+							</div> */}
+							<div class="col">
 								<li className="nav-item ">
-									<button className="btn btn-success btn-sm" data-bs-dismiss="offcanvas" onClick={() => { navigate("/productform") }} aria-expanded="page">
-										Vender<i className="fa-solid fa-up"></i>
+									<button className="btn btn-success " onClick={() => { navigate("/productform") }} aria-expanded="page">
+										Vender<i class="fa-solid fa-up"></i>
 									</button>
 								</li>
 							</div>
 							<div className="col">
 								<li className="nav-item">
 
-									<button type="button" className="btn btn-danger btn-sm " onClick={() => { actions.handleLogout() ? navigate("/") : "" }} >Logout<i className="fa-solid fa-right-from-bracket"></i></button>
+									<button type="button" className="btn btn-danger  " onClick={() => { actions.handleLogout() ? navigate("/") : "" }} >Logout<i class="fa-solid fa-right-from-bracket"></i></button>
 
 								</li>
 							</div>
 						</div>
-					</div>
+					</div></ul>
                         <hr></hr>
-                        <div><h6>Notificaciones</h6></div>
-					<div className="card mb-3">
-						<img src="https://i.blogs.es/352301/switch/1366_2000.webp" className="card-img-top" alt="..." />
-						<div className="card-body">
-							<p className="card-text">Para concretar tu compra debes realizar el pago.</p>
+
+						<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+					<li class="nav-item" role="presentation">
+						<button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true"><i class="fa-regular fa-bell"></i></button>
+					</li>
+					<li class="nav-item" role="presentation">
+						<button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Mis productos</button>
+					</li>
+					
+					</ul>
+					
+						<div class="tab-content" id="pills-tabContent">
+					<div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" tabindex="0">
+
+					<div><h6>Notificaciones</h6></div>
+					<div class="card mb-3 ">
+						<img src="https://i.blogs.es/352301/switch/1366_2000.webp" class="card-img-top" alt="..." />
+						<div class="card-body">
+							<p class="card-text">Para concretar tu compra debes realizar el pago.</p>
 							<button className="btn btn-success btn-sm"> Reportar pago
 							</button>
 						</div>
@@ -67,14 +83,25 @@ export const UserActions = () => {
 							</button>
 						</div>
 					</div>
-				</ul>
-				{store.transacciones.map((transaccion)=>{
+
+					</div>
+					<div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab" tabindex="0">					
+					
+					
+					<div><h6>Mis productos</h6></div>
+					</div>
+					</div>
+					{store.transacciones.map((transaccion)=>{
 					return (
 						<div key={transaccion.id}>
 							{transaccion.transaccion_status}
 						</div>
 					)
 				})}
+
+
+
+
 				</div>
 
 		</div>
